60103bb56198f27be630dd9fca10aa05139e6f96
Winston Underwood <Winston722@gmail.com>
2019-01-22 13:15:44 -0500

    Add files via upload

---
+++ Basics.R
@@ -0,0 +1,133 @@
+####SETUP
+#Author: D. Winston Underwood Jr
+#Date: January 7th, 2019
+#Last Modified: January 7th, 2019
+#Purpose: Basics of R for UBWA
+
+#lines 8-20 install necessary packages. The loop that starts on line 15 checks
+#to see if the package is installed, and installs it if it isnt. Then for each
+#package, it is loaded into the working environment. 
+neededPackages <- c("tidyverse"
+                    , "rvest"
+                    , "rstudioapi"
+                    , "rlang"
+                    #add more packages if needed here
+)
+
+for (i in neededPackages){
+  if( !is.element(i, .packages(all.available = TRUE)) ) {
+    install.packages(i)
+  }
+  library(i,character.only = TRUE)
+}
+
+#this sets the variable 'current_path' equal to whereever this file is open,
+#which is inside the R Workshop folder
+current_path <- getActiveDocumentContext()$path %>% dirname %>% setwd
+
+#we also want a variable called 'wd' that we can reference for our main working
+#directory
+wd <- getwd()
+
+#R can be used as a calculator for arithmatic
+3 + 5
+2^5
+log(3, base = 8)
+
+#R can be used for boolean math, although == is used instead of = for boolean
+#evaluation
+4 + 3 == 8
+TRUE & TRUE
+TRUE | FALSE
+TRUE | FALSE & !TRUE == FALSE    #order of operations matters!
+
+1 == TRUE
+0 == FALSE
+
+#You can assign variables with <- sign, do not use the = sign for assignment
+a <- 5
+b <- 2
+c <- a + b
+
+#how would you print the variable C to the console?
+
+
+
+givenName <- "Winston"
+lastName <- "Underwood"
+#paste combines text strings and seperates them with a space unless you tell it
+#otherwise
+fullName <- paste(givenName,lastName)
+
+#R is most powerful when working with a vector, ie an array which is created
+#with the c() function
+#When working with vectors, operations happen row-wise, ie when adding, the
+#first rows of vectors are added, then the second rows are added, etc
+firstVector <- c(1, 2, 3, 4)
+secondVector <- c(1, 2, 4, 8)
+#add these two vectors together and assign it to variable "thirdVector"
+thirdVector <- firstVector + secondVector
+
+#how would you add 1 to each element of thirdVector? 
+thirdVector + 1
+
+#let's take a look at how piping works
+#These next two lines do the exact same thing
+grades <- as.numeric(    c(92, 81, 55, 100, 101)     )
+grades <- c(92, 81, 55, 100, 101) %>% as.numeric()
+#whether we put the vector inside the as.numeric command, or "pipe" them
+#together, the same action is occuring. Piping is prefered because it can often
+#be read easier because everything is linear: first do x, then do y.
+
+#two different ways to take the mean
+avgGrade <- mean(grades)
+
+#how would we 'pipe' this? 
+avgGrade <- grades %>% mean()
+
+#two different ways to take the median
+medianGrade <- grades %>% median()
+medianGrade <- median(grades)
+
+#if the parenthesis are empty, we don't even need to include them! Although this
+#can sometimes be confusing, because it is not immediately clear if 'median' is
+#a function or a variable
+medianGrade <- grades %>% median
+
+#Data frames are the most useful and powerful structure in R and is essentially
+#a vector of vectors (2D array)
+courses <- as.character(c("Accounting II"
+                          , "Early Roman History"
+                          , "Honors Colours and Shapes"
+                          , "Bayesian Modeling"
+                          , "Spanish III"))
+
+professors <- as.character(c("Mark Wahlberg"
+                            , "Livy"
+                            , "Steve Rogers"
+                            , "Jessica Day"
+                            , "Jose Gonzalez"))
+
+#this combines our vectors by column (cbind) and creates a dataframe from that
+#data, and stores it in the variable "classes" and because it is a dataframe,
+#it's best practice to store it with the suffix ".df"
+classes.df <- as.data.frame(cbind(grades, courses, professors)
+                            , stringsAsFactors = FALSE)
+
+#to select certain columns, we need to know the column names:
+colnames(classes.df)
+
+#this next line transforms the grades column from character type to numeric
+classes.df$grades <- classes.df$grades %>% as.numeric()
+
+#to select certain columns, we use the 'select' function and tell it what column
+#or columns we want
+classes.df %>% select("grades")
+
+classes.df %>% select("grades", "professors")
+
+#Can you figure out what this next line does?
+classes.df %>% filter(grades > 95) %>% select(professors)
+
+#How would you return the names of the classes where your grades are less than 90?
+classes.df %>% filter(grades > 90) %>% select(courses, grades)
---
+++ Main.R
@@ -0,0 +1,126 @@
+####SETUP
+#Author: D. Winston Underwood Jr
+#Date: January 3rd, 2018
+#Last Modified: January 7th, 2019
+#Purpose: Web scraping example for UBWA
+
+#First we need to install the packages we plan on using. Installing only needs
+#to be done once, so if you are using packages that has already been installed,
+#there is no need to install it again.
+
+#The require function returns true if the package is installed and loads it, and
+#returns false if the package is not installed. The if statements below check if
+#a package is installed--if it is, it loads the package. If it isn't, it
+#installs it and loads it.
+
+#The library function does the same thing as require, except it doesn't return a
+#true or false--instead if either loads the package if it is installed, or
+#throws an error if it isn't.
+
+neededPackages <- c("tidyverse"
+                    , "rvest"
+                    , "rstudioapi"
+                    , "rlang"
+                    #add more packages if needed here
+                    )
+
+for (i in neededPackages){
+  if( !is.element(i, .packages(all.available = TRUE)) ) {
+    install.packages(i)
+  }
+  library(i,character.only = TRUE)
+}
+
+#now that we have all the packages installed and loaded, we need to set our
+#working directory, which is a way to tell R where to find the files we
+#reference. Our working directory is going to be the "R Workshop" folder you
+#downloaded. This next block of code uses the rstudioapi library, and will not
+#work if that package was not installed and loaded.
+
+#this sets the variable 'current_path' equal to whereever this file is open,
+#which is inside the R Workshop folder
+#dirname returns the folder location of that file path we just created
+#setwd sets our working directory to that folder path
+current_path <- getActiveDocumentContext()$path %>% dirname() %>% setwd()
+
+#we also want a variable called 'wd' that we can reference for our main working
+#directory
+wd <- getwd()
+
+#import the myMovies file from your working directory. Feel free to create your
+#own list of movies and import that instead / as well, just make sure if you are
+#saving the file to the correct folder with the correct extention (csv)
+
+myMovies.df <- read.csv('Movies.csv')
+myMovies.df
+colnames(myMovies.df) <- "titles"
+
+
+
+
+url <- 'https://www.imdb.com/search/title?groups=top_250&sort=user_rating&count=250'
+con <- url(url,"rb")
+webpage <- read_html(con)
+
+#for title and metacritic score, we'll use piping
+
+titles <- webpage %>%                           #Start with the webpage
+  html_nodes(".lister-item-header a") %>%       #grab these html nodes
+  html_text() %>%                               #translate the nodes to text
+  as.data.frame()                               #store it as a data frame
+
+#metacritic rating
+score <- html_nodes(webpage,".favorable") %>%
+  html_text() %>%
+  as.numeric() %>%
+  as.data.frame()
+
+#for IMBD rating and description we won't use piping
+
+#imdb rating
+rating_html <- html_nodes(webpage,".ratings-imdb-rating")
+rating_text <- html_text(rating_html)
+rating_number <- as.numeric(rating_text)
+rating <- as.data.frame(rating_number)
+
+#movie description
+description_html <- html_nodes(webpage,".ratings-bar+ .text-muted")
+description_text <- html_text(description_html)
+description <- as.data.frame(description_text)
+#has some extra characters we don't want (/n) so this line replaced them with
+#nothing, ie removing them
+description <- gsub("\n","",description)
+
+#we can even pull out director and stars, however we would need regular
+#expressions (regex) to extract those items from the text source
+stars_html <- html_nodes(webpage,".text-muted+ p")
+stars <- html_text(stars_html, trim = TRUE)
+stars <- gsub("\n","",stars) %>% as.data.frame()
+View(stars)
+#now we need to pull everything together and create a single data frame of data
+#with the different columns we have.
+#cbind is the function to bind columns together. rbind is the function to bind
+#rows together
+topMovies.df <- cbind(titles, rating)
+colnames(topMovies.df) <- c("titles", "rating")
+
+#now we have two data frames: topMovies.df has the IMDB top 250 movies, and
+#myMovies.df which has the movies I've seen this year. Our goal is to identify
+#which of them have been seen this year. That requires a join. To do a join, it
+#is helpful if the column names are the same.
+
+#Because our two data frames have one column with the same name, we don't need
+#to specify what columns we want to join by--the inner_join function will auto-
+#matically look for columns with similar names. If you don't have columns with
+#the same name, you'll have to define what the two columns are that are to be
+#connected.
+
+watchedMovies.df <- inner_join(topMovies.df, myMovies.df)
+
+watched <- nrow(myMovies.df)
+watched
+topWatched <- nrow(watchedMovies.df)
+topWatched
+topWatched / watched
+
+watchedMovies.df
